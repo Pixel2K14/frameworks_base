b49be93adee14c7f0631a5c11b1af52908e111e3
Jubakuba <Jubakuba@gmail.com>
2013-12-22 21:45:43 -0700

    Framework: QuickRecord Tile (1/2)
    Images by kroz (thanks!)
    Change-Id: I22f9de88d401a777e987ec2c1bbbf599dc840d5d
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -39,6 +39,7 @@ public class QSConstants {
         public static final String TILE_ONTHEGO = "toggleOnTheGo";
         public static final String TILE_COMPASS = "toggleCompass";
         public static final String TILE_MUSIC = "toggleMusic";
+        public static final String TILE_QUICKRECORD = "toggleQuickRecord";
 
         public static final String TILE_DELIMITER = "|";
         public static ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
