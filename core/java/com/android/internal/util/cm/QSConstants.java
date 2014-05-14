ead47aee134c3a1be4562a6ac029237d5c0a38fa
Jacob Mueller <jacob.mueller.elz@gmail.com>
2014-05-14 22:09:34 +0200

    framework: Add compass tile (1/2)
    Add a compass tile to quick settings.
    Clicking on it enables the sensor and clicking again disables it.
    
    Screenshot: http://goo.gl/2frdpy
    
    Change-Id: Icd9d12929f0491f6958f30de139ef39bb4c2e037
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	core/java/com/android/internal/util/cm/QSUtils.java
    	packages/SystemUI/res/values/cm_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -35,8 +35,9 @@ public class QSConstants {
         public static final String TILE_POWER = "togglePowerMenu";
         public static final String TILE_FASTCHARGE = "toggleFastCharge";
         public static final String TILE_CPUFREQ = "toggleCPUFreq";
-        public static final String TILE_THEME = "toggleTheme"; 
+        public static final String TILE_THEME = "toggleTheme";
         public static final String TILE_ONTHEGO = "toggleOnTheGo";
+        public static final String TILE_COMPASS = "toggleCompass";
 
         public static final String TILE_DELIMITER = "|";
         public static ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
