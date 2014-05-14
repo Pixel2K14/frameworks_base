d093cff18a43c64e2a003e93b9f691b2c5306b5a
Arnav Gosain <nolinuxnoparty@gmail.com>
2014-05-14 14:04:28 -0400

    Fix

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4283,12 +4283,6 @@ public final class Settings {
         public static final String APP_SIDEBAR_SHOW_TRIGGER = "app_sidebar_show_trigger";
 
         /**
-         * Disable FC Notifications
-         * @hide
-         */
-        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
