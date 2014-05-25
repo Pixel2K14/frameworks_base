6ee3428089c152f3bf00eed33e3d45cf050dcdea
Arnav Gosain <nolinuxnoparty@gmail.com>
2014-05-25 03:37:43 -0400

    Revert "[1/2] Base: implement App circle sidebar"
    This reverts commit b195035ad329efc78c8f2693326a48632f0996d4.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1652,7 +1652,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
         } else if (!mRecreating) {
             // usual case: status bar visible & not immersive
-            addAppCircleSidebar();
 
             // show the ticker if there isn't already a heads up
             if (mInterruptingNotificationEntry == null) {
