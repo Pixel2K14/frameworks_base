7f6094286970e4021a5e2921a1c7be3c68751be6
cristianomatos <cristianobmatos@gmail.com>
2014-06-08 14:37:56 -0300

    Revert "Halo: allow choose notifications on floating window or not (1/2)"
    This reverts commit 1840544384f7e32102373b3ecc80fd0320e3e89c.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4435,12 +4435,6 @@ public final class Settings {
         public static final String HALO_UNLOCK_PING = "halo_unlock_ping";
 
         /**
-         * Halo notifications on floating windows or not
-         * @hide
-         */
-        public static final String FLOATING_MODE = "floating_mode";
-
-        /**
          * Show brightness slider in notification panel
          * @hide
          */
