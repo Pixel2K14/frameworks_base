db7ef55c68ce88c88d3dc3436c1862a57966b9fd
Arnav Gosain <nolinuxnoparty@gmail.com>
2014-06-24 07:05:32 -0400

    Revert "HOVER: Fix a couple of things:"
    This reverts commit 688b801b908e1dfb2d021027e74cf9fa41c315bf.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3243,12 +3243,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                             | StatusBarManager.DISABLE_NOTIFICATION_TICKER))) {
                 boolean blacklisted = false;
                 // don't pass notifications that run in Hover to Ticker
-                if (mHoverState == HOVER_ENABLED) {
-                    try {
-                        blacklisted = getNotificationManager().isPackageAllowedForHover(n.getPackageName());
-                    } catch (android.os.RemoteException ex) {
-                        // System is dead
-                    }
+                try {
+                    blacklisted = getNotificationManager().isPackageAllowedForHover(n.getPackageName());
+                } catch (android.os.RemoteException ex) {
+                    // System is dead
                 }
                 if (!blacklisted) mTicker.addEntry(n);
             }
@@ -3268,7 +3266,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     private class MyTicker extends Ticker {
-        private boolean hasTicked = false;
 
         MyTicker(Context context, View sb) {
             super(context, sb);
@@ -3276,28 +3273,24 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         @Override
         public void tickerStarting() {
-            if (mHoverState == HOVER_DISABLED) mTicking = true;
-            mStatusBarContents.setVisibility(View.GONE);
+	    mStatusBarContents.setVisibility(View.GONE);
             mCenterClockLayout.setVisibility(View.GONE);
             mTickerView.setVisibility(View.VISIBLE);
             mTickerView.startAnimation(loadAnim(com.android.internal.R.anim.push_up_in, null));
             mStatusBarContents.startAnimation(loadAnim(com.android.internal.R.anim.push_up_out, null));
             mCenterClockLayout.startAnimation(
             loadAnim(com.android.internal.R.anim.push_up_out, null));
-            hasTicked = true;
         }
 
         @Override
         public void tickerDone() {
-            if (!hasTicked) return;
-            mStatusBarContents.setVisibility(View.VISIBLE);
+	    mStatusBarContents.setVisibility(View.VISIBLE);
             mCenterClockLayout.setVisibility(View.VISIBLE);
             mTickerView.setVisibility(View.GONE);
             mStatusBarContents.startAnimation(loadAnim(com.android.internal.R.anim.push_down_in, null));
             mTickerView.startAnimation(loadAnim(com.android.internal.R.anim.push_down_out,
                         mTickingDoneListener));
             mCenterClockLayout.startAnimation(loadAnim(com.android.internal.R.anim.push_down_in, null));
-            hasTicked = false;
 	}
 
         public void tickerHalting() {
